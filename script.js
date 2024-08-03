cad9c5e0c2468ca651cfdfa0f0c4b8b5ac10b909
Anano Topuria <anano.topuriaa@gmail.com>
2024-08-03 11:12:48 +0400

    feat: add trendyol home page

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggles = document.querySelectorAll(`.acc-toggle`);
+toggles.forEach((toggle) => {
+  toggle.addEventListener(`click`, () => {
+    toggle.parentNode.classList.toggle(`active`);
+  });
+});
